18019dfefdce5ba6cd5662a8b39a90c89c0ea096
biplov <biplovthapa890@gmail.com>
2026-07-16 14:01:22 +0545

    refactor

--- main.go
+++ main.go
@@ -19,18 +19,69 @@ const (
 	dirDown  string = "S"
 )
 
+type gridMetrics struct {
+	padX      int32
+	padY      int32
+	boxW      int32
+	cellCount int32
+	cellSize  int32
+}
+
+func (g *gridMetrics) init(width int32) {
+	g.boxW = width - 2*g.padX
+	g.cellSize = g.boxW / g.cellCount
+}
+
+func (g gridMetrics) drawBox() {
+	green := rl.Color{R: 144, G: 238, B: 144, A: 255}
+	rl.DrawRectangle(g.padX, g.padY, g.boxW, g.boxW, green)
+}
+
+func (g gridMetrics) checkBoundary(newHead Snake) bool {
+	if newHead.X < 0 ||
+		newHead.Y < 0 ||
+		newHead.X > float32(g.cellCount-1) ||
+		newHead.Y > float32(g.cellCount-1) {
+		return true
+	}
+	return false
+}
+
+func (g gridMetrics) eatenOrNot(s []Snake, foodXrand, foodYrand int) bool {
+	return (s[0].X*float32(g.cellSize) <= float32(foodXrand)*float32(g.cellSize)) &&
+		s[0].X*float32(g.cellSize)+float32(g.cellSize) >= float32(foodXrand)*float32(g.cellSize)+
+			float32(g.cellSize/2) &&
+		s[0].Y*float32(g.cellSize) <= float32(foodYrand)*float32(g.cellSize) &&
+		s[0].Y*float32(g.cellSize)+float32(g.cellSize) >= float32(foodYrand)*float32(g.cellSize)+
+			float32(g.cellSize/2)
+}
+
+type motionMetrics struct {
+	speed        float32
+	moveTimer    float32
+	moveInterval float32
+	increment    float32
+}
+type scoreMetrics struct {
+	score    int32
+	posX     int32
+	posY     int32
+	fontSize int32
+	color    rl.Color
+}
+
 func main() {
 	var width int32 = 800
 	var height int32 = 800
 	rl.InitWindow(width, height, "u are a snake")
 	defer rl.CloseWindow()
 	rl.SetTargetFPS(60)
-	var padX int32 = 20
-	var padY int32 = 30
-	boxW := width - 2*padX
-	boxH := boxW
-	var gridNum int32 = 30
-	gridW := boxH / gridNum
+	g := gridMetrics{
+		padX:      20,
+		padY:      30,
+		cellCount: 30,
+	}
+	g.init(width)
 	head := Snake{
 		X: 2,
 		Y: 0,
@@ -38,56 +89,65 @@ func main() {
 	s := make([]Snake, 2)
 	s[0] = head
 	dir := dirRight
-	var speed float32 = 0.5
-	var gridTimer float32 = 0
-	const moveInterval float32 = 0.15
-	xRand := rand.Intn(int(gridNum-2)) + 1
-	yRand := rand.Intn(int(gridNum-2)) + 1
+	m := motionMetrics{
+		speed:        0.5,
+		moveTimer:    0,
+		moveInterval: 0.15,
+		increment:    0.25,
+	}
+	foodXrand := rand.Intn(int(g.cellCount-2)) + 1
+	foodYrand := rand.Intn(int(g.cellCount-2)) + 1
 	gameOver := false
-	score := 0
-	const increment float32 = 0.25
-	headRight := rl.LoadTexture("./headLeft.png")
+	headLeft := rl.LoadTexture("./headLeft.png")
 	tail := rl.LoadTexture("./tail.png")
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
 		rl.ClearBackground(rl.RayWhite)
-		drawBox(padX, padY, boxW, boxH)
-		rl.DrawRectangle(int32(float32(padX)+float32(xRand)*float32(gridW)),
-			int32(float32(padY)+float32(yRand)*float32(gridW)),
-			gridW/2,
-			gridW/2,
+		g.drawBox()
+		rl.DrawRectangle(int32(float32(g.padX)+float32(foodXrand)*float32(g.cellSize)),
+			int32(float32(g.padY)+float32(foodYrand)*float32(g.cellSize)),
+			g.cellSize/2,
+			g.cellSize/2,
 			rl.Yellow)
 		if gameOver {
 			gameOverContent()
 		}
-		rl.DrawText(strconv.Itoa(score), 400, 10, 25, rl.Black)
-		handleKeyEvents(&dir, &score, &head, &s, &speed, &gridTimer, &gameOver)
-		gridTimer += rl.GetFrameTime()
-		if gridTimer > moveInterval {
+		sMet := scoreMetrics{
+			score:    0,
+			posX:     400,
+			posY:     10,
+			fontSize: 25,
+			color:    rl.Black,
+		}
+		rl.DrawText(strconv.Itoa(int(sMet.score)), sMet.posX, sMet.posY,
+			sMet.fontSize, rl.Black)
+		handleKeyEvents(&dir, &sMet, &head, &s, &m, &gameOver)
+		m.moveTimer += rl.GetFrameTime()
+		if m.moveTimer > m.moveInterval {
 			newHead := s[0]
 			switch dir {
 			case dirDown:
-				newHead.Y += speed
+				newHead.Y += m.speed
 			case dirUp:
-				newHead.Y -= speed
+				newHead.Y -= m.speed
 			case dirRight:
-				newHead.X += speed
+				newHead.X += m.speed
 			case dirLeft:
-				newHead.X -= speed
+				newHead.X -= m.speed
 			}
-			if checkSelfCollision(newHead, s) || checkBoundary(newHead, gridNum) {
-				speed = 0
+			if checkSelfCollision(newHead, s) || g.checkBoundary(newHead) {
+				m.speed = 0
 				gameOver = true
-				if checkBoundary(newHead, gridNum) {
+				if g.checkBoundary(newHead) {
 					switch {
 					case newHead.X < 0:
 						newHead.X = 0
-					case newHead.X > float32(gridNum-1):
-						newHead.X = float32(gridNum - 1)
+					case newHead.X > float32(g.cellCount-1):
+						newHead.X = float32(g.cellCount - 1)
 					case newHead.Y < 0:
 						newHead.Y = 0
 					default:
-						newHead.Y = float32(gridNum - 1)
+						newHead.Y = float32(g.cellCount - 1)
 					}
 				}
 				s = append([]Snake{newHead}, s...)
@@ -96,44 +156,37 @@ func main() {
 				s = s[:len(s)-1]
 				head = newHead
 			}
-			gridTimer = 0
+			m.moveTimer = 0
 		}
 		for i := range s {
 			if i == 0 {
-				handleLoop(headRight, dir, gridW, s, padX, padY, i, "head")
+				g.handleLoop(headLeft, dir, s, i, "head")
 				continue
 			} else if i == len(s)-1 {
-				handleLoop(tail, dir, gridW, s, padX, padY, i, "tail")
+				g.handleLoop(tail, dir, s, i, "tail")
 				continue
 			}
-			rl.DrawRectangle(int32(float32(padX)+s[i].X*float32(gridW)),
-				int32(float32(padY)+s[i].Y*float32(gridW)),
-				gridW,
-				gridW,
+			rl.DrawRectangle(int32(float32(g.padX)+s[i].X*float32(g.cellSize)),
+				int32(float32(g.padY)+s[i].Y*float32(g.cellSize)),
+				g.cellSize,
+				g.cellSize,
 				rl.Red)
 		}
-		if (s[0].X*float32(gridW) <= float32(xRand)*float32(gridW)) &&
-			s[0].X*float32(gridW)+float32(gridW) >= float32(xRand)*float32(gridW)+float32(gridW/2) &&
-			s[0].Y*float32(gridW) <= float32(yRand)*float32(gridW) &&
-			s[0].Y*float32(gridW)+float32(gridW) >= float32(yRand)*float32(gridW)+float32(gridW/2) {
-			xRand = rand.Intn(int(gridNum-2)) + 1
-			yRand = rand.Intn(int(gridNum-2)) + 1
+		if g.eatenOrNot(s, foodXrand, foodYrand) {
+			foodXrand = rand.Intn(int(g.cellCount-2)) + 1
+			foodYrand = rand.Intn(int(g.cellCount-2)) + 1
 			s = append(s, head)
-			if speed < 1 {
-				speed += increment
+			if m.speed < 1 {
+				m.speed += m.increment
 			}
-			score++
+			sMet.score++
 		}
 		rl.EndDrawing()
 	}
 }
 
-func drawBox(padX, padY, boxW, boxH int32) {
-	green := rl.Color{R: 144, G: 238, B: 144, A: 255}
-	rl.DrawRectangle(padX, padY, boxW, boxH, green)
-}
-
 func checkSelfCollision(newHead Snake, s []Snake) bool {
+	// TODO:
 	for i := 1; i < len(s); i++ {
 		if newHead.X == s[i].X && newHead.Y == s[i].Y {
 			return true
@@ -142,29 +195,37 @@ func checkSelfCollision(newHead Snake, s []Snake) bool {
 	return false
 }
 
-func checkBoundary(newHead Snake, gridNum int32) bool {
-	if newHead.X < 0 || newHead.Y < 0 || newHead.X > float32(gridNum-1) || newHead.Y > float32(gridNum-1) {
-		return true
-	}
-	return false
+type gameOverMets struct {
+	textX    int32
+	textY    int32
+	rectX    int32
+	rectY    int32
+	rectW    int32
+	rectH    int32
+	textFont int32
+	posX     int32
+	posY     int32
 }
 
 func gameOverContent() {
-	var textX int32 = 50
-	var textY int32 = 10
-	var rectX int32 = 600
-	var rectY int32 = 10
-	var rectW int32 = 175
-	var rectH int32 = 30
-	var textFont int32 = 25
-	var posX int32 = 630
-	var posY int32 = 14
-	rl.DrawText("Game is over", textX, textY, textFont, rl.Black)
-	rl.DrawRectangle(rectX, rectY, rectW, rectH, rl.Black)
-	rl.DrawText("press n", posX, posY, textFont, rl.LightGray)
+	gOver := gameOverMets{
+		textX:    50,
+		textY:    10,
+		rectX:    600,
+		rectY:    10,
+		rectW:    175,
+		rectH:    30,
+		textFont: 25,
+		posX:     630,
+		posY:     14,
+	}
+	rl.DrawText("Game is over", gOver.textX, gOver.textY,
+		gOver.textFont, rl.Black)
+	rl.DrawRectangle(gOver.rectX, gOver.rectY, gOver.rectW, gOver.rectH, rl.Black)
+	rl.DrawText("press n", gOver.posX, gOver.posY, gOver.textFont, rl.LightGray)
 }
 
-func handleKeyEvents(dir *string, score *int, head *Snake, s *[]Snake, speed *float32, gridTimer *float32, gameOver *bool) {
+func handleKeyEvents(dir *string, sMet *scoreMetrics, head *Snake, s *[]Snake, m *motionMetrics, gameOver *bool) {
 	switch rl.GetKeyPressed() {
 	case rl.KeyS:
 		if *dir != dirUp {
@@ -188,21 +249,16 @@ func handleKeyEvents(dir *string, score *int, head *Snake, s *[]Snake, speed *fl
 			X: 2,
 			Y: 0,
 		}
-		// *second = Snake{
-		// 	X: 1,
-		// 	Y: 0,
-		// }
 		*dir = dirRight
 		(*s)[0] = *head
-		// (*s)[1] = *second
-		*speed = 0.5
-		*gridTimer = 0
+		m.speed = 0.5
+		m.moveTimer = 0
 		*gameOver = false
-		*score = 0
+		sMet.score = 0
 	}
 }
 
-func handleLoop(headOrTail rl.Texture2D, dir string, gridW int32, s []Snake, padX, padY int32, i int, somename string) {
+func (g gridMetrics) handleLoop(headOrTail rl.Texture2D, dir string, s []Snake, i int, somename string) {
 	tailDir := headOrTail
 	var rotation float32 = 0
 	var offsetX float32 = 0
@@ -213,15 +269,15 @@ func handleLoop(headOrTail rl.Texture2D, dir string, gridW int32, s []Snake, pad
 	switch dir {
 	case dirLeft:
 		rotation = 0
-		offsetX += float32(gridW)
-		offsetY += float32(gridW)
+		offsetX += float32(g.cellSize)
+		offsetY += float32(g.cellSize)
 	case dirRight:
 		rotation = 180
 	case dirUp:
-		offsetY += float32(gridW)
+		offsetY += float32(g.cellSize)
 		rotation = 90
 	case dirDown:
-		offsetX += float32(gridW)
+		offsetX += float32(g.cellSize)
 		offsetY -= 2
 		rotation = -90
 	}
@@ -232,10 +288,10 @@ func handleLoop(headOrTail rl.Texture2D, dir string, gridW int32, s []Snake, pad
 		Width:  float32(tailDir.Width),
 	}
 	destRec := rl.Rectangle{
-		X:      float32(padX) + s[i].X*float32(gridW) + float32(offsetX),
-		Y:      float32(padY) + s[i].Y*float32(gridW) + float32(offsetY),
-		Width:  float32(gridW),
-		Height: float32(gridW),
+		X:      float32(g.padX) + s[i].X*float32(g.cellSize) + float32(offsetX),
+		Y:      float32(g.padY) + s[i].Y*float32(g.cellSize) + float32(offsetY),
+		Width:  float32(g.cellSize),
+		Height: float32(g.cellSize),
 	}
 	origin := rl.Vector2{X: destRec.Width, Y: destRec.Height}
 	rl.DrawTexturePro(tailDir, srcRec, destRec, origin, rotation, rl.White)
